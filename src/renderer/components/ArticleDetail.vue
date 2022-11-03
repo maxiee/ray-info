2ffc15b2e0e3bacc7440786fd2ac03a3cbfa6815
maxiee <maxieewong@gmail.com>
2022-11-04 01:16:34 +0800

    article detail

--- src/renderer/components/ArticleDetail.vue
+++ src/renderer/components/ArticleDetail.vue
@@ -1,5 +1,5 @@
 <template>
-    <pane min-size="45">
+    <pane min-size="45" class="article-detail">
         <div>
             <h2>Article Detail</h2>
             <article-toolbar/>
@@ -13,6 +13,17 @@ import {Pane} from 'splitpanes';
 export default {
     components: {
         Pane
+    },
+    data() {
+        return {}
     }
 }
 </script>
+
+<style>
+.article-detail {
+    position: relative;
+    flex-grow: 1;
+    height: 100%;
+}
+</style>
